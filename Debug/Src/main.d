23764f80a34afe71b9213083c47fb80b4d65f165
mateo721 <mateo-a721@hotmail.com>
2020-06-18 15:08:19 -0500

    Se crea proyecto y se monta el antirebote

---
+++ Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c 
+
+OBJS += \
+./Src/main.o 
+
+C_DEPS += \
+./Src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/main.o: ../Src/main.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32L4 -DSTM32 -DDEBUG -DSTM32L476RGTx -c -I"C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Include" -I"C:/Users/Asus-Pc/STM32CubeIDE/workspace_1.3.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
